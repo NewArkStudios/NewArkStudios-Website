ee11e1795fb77ca04831da1248963eeff63c06f0
lantern77 <peter_hoang2@hotmail.com>
2017-06-25 13:00:18 -0400

    new mod panel info now displays reports

--- app/Models/Report.php
+++ app/Models/Report.php
@@ -11,4 +11,46 @@ class Report extends Model
 
     // columns in table that we are allowed to fill
     protected $fillable = ['reason'];
+
+    /**
+    * Get the user who reported this individual
+    */
+    public function reporter() {
+        return $this->hasOne('App\User','id', 'reporter_id');
+    }
+    
+    /**
+    * Get the user who reported this individual
+    */
+    public function suspect() {
+        return $this->hasOne('App\User','id', 'suspect_id');
+    }
+
+    /**
+    * Get the post in question
+    */
+    public function post() {
+        return $this->hasOne('App\Models\Post','id', 'post_id');
+    }
+    
+    /**
+    * Get the reply in question
+    */
+    public function reply() {
+        return $this->hasOne('App\Models\Reply','id', 'reply_id');
+    }
+
+    /**
+    * Get the direct message in question
+    */
+    public function message() {
+        return $this->hasOne('App\Models\Message','id', 'message_id');
+    }
+
+    /**
+    * Get the direct message reply in question
+    */
+    public function messagereply() {
+        return $this->hasOne('App\Models\MessageReply','id', 'messagereply_id');
+    }
 }
--- resources/views/pages/moderator_panel.blade.php
+++ resources/views/pages/moderator_panel.blade.php
@@ -6,12 +6,65 @@
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
                 <div class="panel-heading">This is the moderator panel</div>
+                <!-- This will change of course -->
+                <!--Suggestion might use Jquery datatables-->
                 <div class="panel-body">
-                        @foreach($reports as $report)
-                        <div class='col-md-12'>
-                            {{$report->reason}}
-                        <div>
-                        @endforeach
+                    @foreach($reports as $report)
+                        <div class="col-md-12 well">
+                            <p>Number: {{$loop->iteration}}</p>
+                            <p>Reason: {{$report->reason}}</p>
+                            <p>Reporter: {{$report->reporter->name}}</p>
+                            <p>Suspect: {{$report->suspect->name}}</p>
+                            <!-- If there is a post show the post -->
+                            @if(!is_null($report->post_id))
+                                Post in Question:
+                                    <a href="{{url('/post' . $report->post->slug)}}">
+                                        {{$report->post->title}}
+                                    </a>
+                            @endif
+                            <!-- TODO we may need to start linking sections# later -->
+                            @if(!is_null($report->reply_id))
+                                Reply in Question:
+                                    <a href="{{url('/post' . $report->reply->post->slug)}}">
+                                        <!--TODO Some point make js function to reduce -->
+                                        {{$report->reply->body}}
+                                    </a>
+                            @endif
+                            @if(!is_null($report->message_id))
+                                Direct Message in Question:
+                                    <!--TODO Some point make js function to reduce -->
+                                    {{$report->message->body}}
+                            @endif
+                            @if(!is_null($report->messagereply_id))
+                                Direct Message in Question:
+                                    <!--TODO Some point make js function to reduce -->
+                                    {{$report->messagereply->body}}
+                            @endif
+                            <br>
+                            <br>
+                            <form style="display:inline-table;" role="form" method="POST" action="{{ url('/display_report_user') }}">
+                                {{ csrf_field() }}
+                                <input type="hidden" name="suspect_id" value="{{$report->suspect_id}}"></input>
+                                <button type="submit" class="btn btn-danger">
+                                    Permanent Ban User
+                                </button>
+                            </form>
+                            <form style="display:inline-table;" role="form" method="POST" action="{{ url('/display_report_user') }}">
+                                {{ csrf_field() }}
+                                <input type="hidden" name="suspect_id" value="{{$report->suspect_id}}"></input>
+                                <button type="submit" class="btn btn-danger">
+                                    Temporary Ban Users
+                                </button>
+                            </form>
+                            <form style="display:inline-table;" role="form" method="POST" action="{{ url('/display_report_user') }}">
+                                {{ csrf_field() }}
+                                <input type="hidden" name="suspect_id" value="{{$report->suspect_id}}"></input>
+                                <button type="submit" class="btn btn-danger">
+                                    Warn User
+                                </button>
+                            </form>
+                        </div>
+                    @endforeach
                 </div>
             </div>
         </div>
