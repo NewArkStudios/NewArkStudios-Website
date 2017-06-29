b5653c114d7548ec0f88a7f629e2d8d4a436bf84
lantern77 <peter_hoang2@hotmail.com>
2017-06-29 18:20:32 -0400

    cosmetic update to web routes, just easier to read and moved middleware around

--- routes/web.php
+++ routes/web.php
@@ -25,62 +25,6 @@ Route::post('display_report_user', 'ReportController@display_report_user')
 ->middleware('logged_in');
 
 /**
-* Grouped routing for threads
-*/
-Route::group(['prefix' => 'thread'], function(){
-
-    // Route for making post UI Wise
-    Route::get('view_categories', 'ForumController@view_categories')->name('view_categories');
-
-    // Route for submitting forum thread
-    Route::post('make_post', 'PostController@make_post')
-    ->name('make_post')->middleware('logged_in');
-
-    // Route for replying to forum post
-    Route::post('make_reply', 'ReplyController@make_reply')
-        ->name('make_reply')->middleware('logged_in');
-
-    // close a post so no one can contribute to it anymore
-    Route::post('close_post', 'PostController@close_post')->name('close_post');
-
-    // close a post so no one can contribute to it anymore
-    Route::post('open_post', 'PostController@open_post')->name('open_post');
-
-    // close a post so no one can contribute to it anymore
-    Route::post('delete_post', 'PostController@delete_post')->name('delete_post');
-
-    // Post request to actually send report
-    Route::post('report_user', 'ReportController@report_user')
-    ->middleware('logged_in')->name('report_user');
-});
-
-/**
-* Grouped routing for sending direct messages
-*/
-Route::group(['prefix' => 'messages'], function(){
-    
-    // Route for getting the UI to send a direct message
-    Route::get('display_make_message/{receiver_name}',
-        'MessageController@display_make_message')->middleware('logged_in');
-
-    // Route for sending a direct message
-    Route::post('make_message', 'MessageController@make_message')
-        ->name('make_message')->middleware('logged_in');
-
-    // Get inbox, list all messages
-    Route::get('inbox', 'MessageController@inbox')
-        ->name('inbox')->middleware('logged_in');
-        
-    // Get individual Direct message to display
-    Route::get('message/{message_id}', 'MessageController@display_message')
-    ->middleware('logged_in');
-
-    // Route for sending a direct message
-    Route::post('reply_message', 'MessageController@reply_message')
-        ->name('reply_message')->middleware('logged_in');
-});
-
-/**
 * Routing for threading categories, display all posts under the category
 * TODO possibly do pinned posts
 */
@@ -97,6 +41,73 @@ Route::get('/thanks_post', function() {
     return view('pages.thread_thanks_post');
 });
 
+// Grab the profile to display
+Route::get('profile/{profile_slug}', 'AccountController@display_profile');
+
+// Panel for special admin management
+Route::get('admin_panel', 'AdminController@display_admin_panel')->name('display_admin_panel');
+/**
+* Grouped routing for threads
+*/
+Route::group(['prefix' => 'thread'], function(){
+
+    // Route for submitting forum thread
+    Route::post('make_post', 'PostController@make_post')
+    ->name('make_post')->middleware('logged_in');
+
+    // Route for replying to forum post
+    Route::post('make_reply', 'ReplyController@make_reply')
+        ->name('make_reply')->middleware('logged_in');
+
+    // Post request to actually send report
+    Route::post('report_user', 'ReportController@report_user')
+    ->middleware('logged_in')->name('report_user');
+
+    // Route for making post UI Wise
+    Route::get('view_categories', 'ForumController@view_categories')
+    ->name('view_categories')->middleware('moderator');
+
+    // close a post so no one can contribute to it anymore
+    Route::post('close_post', 'PostController@close_post')
+    ->name('close_post')->middleware('moderator');
+
+    // close a post so no one can contribute to it anymore
+    Route::post('open_post', 'PostController@open_post')
+    ->name('open_post')->middleware('moderator');
+
+    // close a post so no one can contribute to it anymore
+    Route::post('delete_post', 'PostController@delete_post')
+    ->name('delete_post')->middleware('moderator');
+
+});
+
+/**
+* Grouped routing for sending direct messages
+*/
+Route::group(['prefix' => 'messages', 'middleware' => ['logged_in']], function(){
+    
+    // Route for getting the UI to send a direct message
+    Route::get('display_make_message/{receiver_name}',
+        'MessageController@display_make_message');
+
+    // Route for sending a direct message
+    Route::post('make_message', 'MessageController@make_message')
+        ->name('make_message');
+
+    // Get inbox, list all messages
+    Route::get('inbox', 'MessageController@inbox')
+        ->name('inbox');
+        
+    // Get individual Direct message to display
+    Route::get('message/{message_id}', 'MessageController@display_message');
+
+    // Route for sending a direct message
+    Route::post('reply_message', 'MessageController@reply_message')
+        ->name('reply_message');
+});
+
+
+
 
 /**
 * Grouped routing for threads for account handling
@@ -105,28 +116,17 @@ Route::group(['prefix' => 'account'], function(){
 
     // Route for viewing profile information
     Route::get('profile', 'AccountController@display_edit_profile')->name('update_profile');
-
     Route::post('update_profile_post', 'AccountController@update_profile_post')->name('update_profile_post');
-
     Route::get('settings', 'AccountController@get_Settings')->name('account_settings');
-
     Route::post('update_account_post', 'AccountController@update_account_post')->name('update_account_post');
-
 });
 
 /**
 * Route group for moderator actions 
 **/
 Route::group(['prefix' => 'moderator', 'middleware' => ['moderator']], function(){
-
     Route::get('display_moderator_panel', 'ModeratorController@display_moderator_panel')->name('display_moderator_panel');
-
     Route::post('ban_user', 'ModeratorController@ban_user')->name('ban_user');
     Route::post('warn_user', 'ModeratorController@warn_user')->name('warn_user');
     Route::post('suspend_user', 'ModeratorController@suspend_user')->name('suspend_user');
 });
-
-Route::get('profile/{profile_slug}', 'AccountController@display_profile');
-
-// Panel for special admin management
-Route::get('admin_panel', 'AdminController@display_admin_panel')->name('display_admin_panel');
