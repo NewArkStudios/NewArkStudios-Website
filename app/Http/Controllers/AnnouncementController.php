1cd96bd0e2ed57d8dc6d85b952361f0dff9e275a
lantern77 <peter_hoang2@hotmail.com>
2017-07-20 19:25:02 -0400

    Slow day today, paginated announcements

--- app/Http/Controllers/AnnouncementController.php
+++ app/Http/Controllers/AnnouncementController.php
@@ -10,7 +10,7 @@ class AnnouncementController extends Controller
     // display announcements on page
     public function display_announcements() {
 
-        $announcements = Announcement::paginate(4);
+        $announcements = Announcement::paginate(5);
         return view('pages.announcements', compact('announcements'));
         
     }
