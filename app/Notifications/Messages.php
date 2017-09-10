adb5d74d75086bdf77c2778cb46e13f799c96eb9
lantern77 <peter_hoang2@hotmail.com>
2017-09-10 11:16:24 -0400

    Update database skeleton

--- app/Notifications/Messages.php
+++ app/Notifications/Messages.php
@@ -29,21 +29,7 @@ class Messages extends Notification
      */
     public function via($notifiable)
     {
-        return ['mail'];
-    }
-
-    /**
-     * Get the mail representation of the notification.
-     *
-     * @param  mixed  $notifiable
-     * @return \Illuminate\Notifications\Messages\MailMessage
-     */
-    public function toMail($notifiable)
-    {
-        return (new MailMessage)
-                    ->line('The introduction to the notification.')
-                    ->action('Notification Action', url('/'))
-                    ->line('Thank you for using our application!');
+        return ['database'];
     }
 
     /**
@@ -55,7 +41,8 @@ class Messages extends Notification
     public function toArray($notifiable)
     {
         return [
-            //
+            'message' => 'A new post was published on Laravel News.',
+            'action' => url($this->post->slug)
         ];
     }
 }
