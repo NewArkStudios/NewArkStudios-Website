bbdfdb48eab3c3980b403bd68819c288a97797b8
lantern77 <peter_hoang2@hotmail.com>
2017-09-12 18:02:30 -0400

    Cosmetic spacing

--- resources/views/layouts/partials/nav.blade.php
+++ resources/views/layouts/partials/nav.blade.php
@@ -33,31 +33,31 @@
                 <li><a href="{{ route('register') }}">Register</a></li>
             @else
                 <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                {{ Auth::user()->name }} <span class="caret"></span>
-                </a>
-                <ul class="dropdown-menu" role="menu">
-                <li>
-                <a href="{{url('profile/' . Auth::user()->name)}}">Profile</a>
-                <a href="{{route('inbox')}}">Inbox</a>
-                <a href="{{route('update_profile')}}">Update Profile</a>
-                <a href="{{route('account_settings')}}">Update Account</a>
-                @if(Auth::user()->hasRole('admin'))
-                <a href="{{route('display_admin_panel')}}">Admin Panel</a>
-                @endif
-                @if(Auth::user()->hasRole('moderator'))
-                  <a href="{{route('display_moderator_panel')}}">Mod Panel</a>
-                @endif
-                <a href="{{ route('logout') }}"
-                onclick="event.preventDefault();
-                document.getElementById('logout-form').submit();">
-                Logout
-                </a>
-                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                {{ csrf_field() }}
-                </form>
-                </li>
-                </ul>
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                    {{ Auth::user()->name }} <span class="caret"></span>
+                    </a>
+                    <ul class="dropdown-menu" role="menu">
+                        <li>
+                            <a href="{{url('profile/' . Auth::user()->name)}}">Profile</a>
+                            <a href="{{route('inbox')}}">Inbox</a>
+                            <a href="{{route('update_profile')}}">Update Profile</a>
+                            <a href="{{route('account_settings')}}">Update Account</a>
+                            @if(Auth::user()->hasRole('admin'))
+                            <a href="{{route('display_admin_panel')}}">Admin Panel</a>
+                            @endif
+                            @if(Auth::user()->hasRole('moderator'))
+                              <a href="{{route('display_moderator_panel')}}">Mod Panel</a>
+                            @endif
+                            <a href="{{ route('logout') }}"
+                            onclick="event.preventDefault();
+                            document.getElementById('logout-form').submit();">
+                            Logout
+                            </a>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            {{ csrf_field() }}
+                            </form>
+                        </li>
+                    </ul>
                 </li>
             @endif
 
