3185b7939b107c0283b71109ab9f613286883d95
lantern77 <peter_hoang2@hotmail.com>
2017-09-17 11:03:47 -0400

    Comment of app.js

--- public/js/app/app.js
+++ public/js/app/app.js
@@ -1,17 +1,22 @@
+/**
+* Module used to run on every page of the website
+* 
+*js/app/app.js
+*/
 define(['jquery'],function($){
 
     var app = {
     
+        /**
+        * Function to run when the page starts
+        */
         'start' : function() {
 
             $.get( "/get_all_notifications", function( data ) {
                 console.log(data);
             });
-        
         }
-
     }
 
     return app;
-
 });
