58c7d1b7f5c59a5e642ee1a1696cb7ab82ed61ac
lantern77 <peter_hoang2@hotmail.com>
2017-10-29 11:14:14 -0400

    Editor now added to edit reply section

--- resources/views/pages/thread_edit_reply.blade.php
+++ resources/views/pages/thread_edit_reply.blade.php
@@ -16,9 +16,9 @@
                             <!-- Make sure that we check user has right to edit -->
                             <input type="hidden" name="reply_id" value="{{$reply->id}}"></input>
                             <div class="form-group{{ $errors->has('body') ? ' has-error' : '' }}">
-                                <label for="body" class="col-md-4 control-label">Body</label>
-                                <div class="col-md-6">
-                                    <textarea id="body" value="{{$reply->body}}" class="form-control" name="body" required>{{ $reply->body }}</textarea>
+                                <label for="body" class="col-md-2 control-label">Body</label>
+                                <div class="col-md-10">
+                                    <textarea rows="15" id="body" value="{{$reply->body}}" class="form-control" name="body" required>{{ $reply->body }}</textarea>
 
                                     @if ($errors->has('body'))
                                         <span class="help-block">
@@ -28,7 +28,7 @@
                                 </div>
                             </div>
                             <div class="form-group">
-                                <div class="col-md-6 col-md-offset-4">
+                                <div class="col-md-10 col-md-offset-2">
                                     <button type="submit" class="btn btn-primary">
                                         Post
                                     </button>
@@ -42,4 +42,7 @@
         </div>
     </div>
 </div>
+@endsection
+@section('custom-javascripts')
+    <script src="{{ asset('js/app/app.thread_edit_reply.js') }}"></script>
 @endsection
