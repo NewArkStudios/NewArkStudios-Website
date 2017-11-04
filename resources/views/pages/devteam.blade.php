31cc270ae423a4b71c6804de0cd785a8112b2639
lantern77 <peter_hoang2@hotmail.com>
2017-11-04 11:36:11 -0400

    Small fix for ui of dev team page

--- resources/views/pages/devteam.blade.php
+++ resources/views/pages/devteam.blade.php
@@ -1,12 +1,14 @@
 @extends('layouts.masters.main')
 
 @section('page-content')
-<div class="jumbotron">
-    <div class="row">
-        <div class="panel panel-default">
-        <h1>Dev team</h1>
-        <div class='col-md-4'>
-           <h2>tests</h2> 
+<div class="container">
+    <div class="jumbotron">
+        <div class="row">
+            <div class="panel panel-default">
+            <h1>Dev team</h1>
+            <div class='col-md-4'>
+               <h2>tests</h2> 
+            </div>
         </div>
     </div>
 </div>
