639b423d5cdf25fea9690123c42a9166408b5c55
lantern77 <peter_hoang2@hotmail.com>
2017-11-19 13:17:04 -0500

    temp under construction image

--- resources/views/pages/display_all_games.blade.php
+++ resources/views/pages/display_all_games.blade.php
@@ -25,7 +25,7 @@
                 'pages.partials.game_profile',
                     [
                         "game_title" => "Game title 2",
-                        "game_img" => "",
+                        "game_img" => "/img/general/under_construction.jpg",
                         "game_description" => "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
                         "twitter" => "bob",
                         "youtube" => "bob",
