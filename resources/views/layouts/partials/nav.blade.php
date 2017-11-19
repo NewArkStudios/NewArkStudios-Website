8f4b7b8007145d616ed5bf7295070b9148b38a88
lantern77 <peter_hoang2@hotmail.com>
2017-11-19 12:35:22 -0500

    fixed navbar to use container-fluid, less overflow

--- resources/views/layouts/partials/nav.blade.php
+++ resources/views/layouts/partials/nav.blade.php
@@ -1,5 +1,5 @@
 <nav id="global-navbar" class="navbar navbar-inverse navbar-fixed-top">
-      <div class="container">
+      <div class="container-fluid">
         <div class="navbar-header">
           <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
             <span class="sr-only">Toggle navigation</span>
