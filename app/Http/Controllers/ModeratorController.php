a4568c1ef051ea06e5e48f4362ba09b2f67a351b
lantern77 <peter_hoang2@hotmail.com>
2017-11-26 18:08:59 -0500

    How did I forget the class import

--- app/Http/Controllers/ModeratorController.php
+++ app/Http/Controllers/ModeratorController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Report;
 use App\Models\Message;
+use App\Models\Post;
 use App\Users;
 use Illuminate\Support\Facades\Auth;
 use \Carbon\Carbon;
