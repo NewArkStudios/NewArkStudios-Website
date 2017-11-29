05d6e4911ac94136d379be40be8acdd029c7b9dc
lantern77 <peter_hoang2@hotmail.com>
2017-11-28 20:23:39 -0500

    temp foldrum build, hopefully I can optimize and style

--- resources/views/pages/demo_foldrum.blade.php
+++ resources/views/pages/demo_foldrum.blade.php
@@ -6,32 +6,24 @@
 @endsection
 
 @section('page-content')
-    <title>Unity WebGL Player | SpaceAttack</title>
-    <link rel="stylesheet" href="TemplateData/style.css">
-    <link rel="shortcut icon" href="TemplateData/favicon.ico" />
-    <script src="TemplateData/UnityProgress.js"></script>
-  <body class="template">
-    <p class="header"><span>Unity WebGL Player | </span>SpaceAttack</p>
-    <div class="template-wrap clear">
-      <canvas class="emscripten" id="canvas" oncontextmenu="event.preventDefault()" height="600px" width="960px"></canvas>
-      <br>
-      <div class="logo"></div>
-      <div class="fullscreen"><img src="TemplateData/fullscreen.png" width="38" height="38" alt="Fullscreen" title="Fullscreen" onclick="SetFullscreen(1);" /></div>
-      <div class="title">SpaceAttack</div>
-    </div>
-    <p class="footer">&laquo; created with <a href="http://unity3d.com/" title="Go to unity3d.com">Unity</a> &raquo;</p>
-    <script type='text/javascript'>
-      var Module = {
-        TOTAL_MEMORY: 268435456,
-        errorhandler: null,         // arguments: err, url, line. This function must return 'true' if the error is handled, otherwise 'false'
-        compatibilitycheck: null,
-        dataUrl: "Release/webgl.data",
-        codeUrl: "Release/webgl.js",
-        memUrl: "Release/webgl.mem",
-      };
-    </script>
-    <script src="Release/UnityLoader.js"></script>
 
+    <title>Unity WebGL Player | Foldrum Offical</title>
+    <link rel="shortcut icon" href="TemplateData/favicon.ico">
+    <link rel="stylesheet" href="TemplateData/style.css">
+    <script src="TemplateData/UnityProgress.js"></script>  
+    <script src="Build/UnityLoader.js"></script>
+    <script>
+      var gameInstance = UnityLoader.instantiate("gameContainer", "Build/Desktop.json", {onProgress: UnityProgress});
+    </script>
+  <body>
+    <div class="webgl-content">
+      <div id="gameContainer" style="width: 960px; height: 600px"></div>
+      <div class="footer">
+        <div class="webgl-logo"></div>
+        <div class="fullscreen" onclick="gameInstance.SetFullscreen(1)"></div>
+        <div class="title">Foldrum Offical</div>
+      </div>
+    </div>
   </body>
 @endsection
 @section('custom-javascripts')
