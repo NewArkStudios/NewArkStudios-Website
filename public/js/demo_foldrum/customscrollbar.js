a25633aee2d95f8d7cb614fa3d105d022e60ca12
lantern77 <peter_hoang2@hotmail.com>
2017-12-02 11:27:54 -0500

    Basic code for custom scroll bar

--- public/js/demo_foldrum/customscrollbar.js
+++ public/js/demo_foldrum/customscrollbar.js
@@ -1,2 +1,14 @@
 console.log($);
 console.log(gameInstance);
+var downloadDone = setInterval(function(){
+    if (gameInstance.Module.buildDownloadProgress.downloadData.finished){
+        clearInterval(downloadDone)
+        console.log(gameInstance.Module.buildDownloadProgress.downloadData.finished)
+
+        // hack unload time is complete when cursor default        
+        var unloadDone = setInterval(function(){
+           // there is point in unload where code just halts
+           console.log('test');
+        }, 1000);
+    }
+}, 1000);
