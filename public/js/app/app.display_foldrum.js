7e4b1ab4dd3cfa7267ef5d6be27267aed220ad30
lantern77 <peter_hoang2@hotmail.com>
2017-12-10 10:59:14 -0500

    Fix for scroll on mobile

--- public/js/app/app.display_foldrum.js
+++ public/js/app/app.display_foldrum.js
@@ -14,8 +14,108 @@ define('app.display_all_games', ['jquery', 'anime'], function($, Anime){
         	var self = this;
 
         	self.animations();
+
+        	// Trigger animations through scroll
+			$(window).on('scroll', self.onScroll.bind(self)); 
         },
 
+        /**
+		* Function to call when scrolling
+        */
+        'onScroll' : function(){
+
+        	// alias
+        	var self = this;
+
+			if ($(window).scrollTop() + 500 > $("#main-description").position().top && !self.triggerAnimations['main-description']) {
+
+			 	// animate main description, 
+			 	Anime({
+				  targets: '#main-description div.left-cont',
+				  translateX: [-500, 0],
+				  duration: 6000,
+				  elasticity: 400,
+				});
+
+				Anime({
+				  targets: '#main-description div.right-cont',
+				  translateX: [1000, 0],
+				  duration: 6000,
+				  elasticity: 200,
+				});
+
+				self.triggerAnimations['main-description'] = true;
+			} 
+
+			 // mechanics section
+			 if ($(window).scrollTop() + 500 > $("#mechanics-title").position().top && !self.triggerAnimations['mechanics']){
+
+			 	// animation timeline
+			 	var onebyoneTimeline = Anime.timeline();
+				$('#mechanics-section div.feature-smallsection').each(function(index){
+	    		 	onebyoneTimeline.add({
+					  targets: '#mechanics-section div.feature-smallsection:nth-of-type(' + (index + 1) + ')',
+					  translateY: {
+					      value : [1000, 0],
+					      duration: 2000,
+					  	  elasticity: 500,
+					  },
+					  opacity : {
+					  	  value : [0, 1],
+					  	  duration: 6000,
+					  },
+					  offset: 1000 * index
+					})
+	    		 });
+
+			 	self.triggerAnimations['mechanics'] = true;
+			}
+
+			if ($(window).scrollTop() + 500 > $("#gallery-section").position().top && !self.triggerAnimations['gallery']){
+
+			 	// animation timeline
+			 	var onebyoneTimelineRotate = Anime.timeline();
+
+			 	// loop through all gallery elements
+			 	$('#gallery-section div.gallery_element').each(function(index){
+					onebyoneTimelineRotate.add({
+					  targets: '#gallery-section div.gallery_element:nth-of-type(' + (index + 1) + ')',
+					  rotate: {
+					      value : [360, 0],
+					      duration: 2000,
+					  	  elasticity: 500,
+					  },
+					  opacity : {
+					  	  value : [0, 1],
+					  	  duration: 6000,
+					  },
+					  scale : {
+					  	  value : [0, 1],
+					  	  duration: 6000,
+					  },
+					  offset: 300 * index 
+					})
+			 	})
+
+			 	self.triggerAnimations['gallery'] = true;
+			}
+
+			if ($(window).scrollTop() + 500 > $("#bottom-banner").position().top && !self.triggerAnimations['bottom']){
+
+			 	Anime({
+				  targets: '#bottom-banner h1, #bottom-banner h4, #bottom-banner button',
+				  opacity: [0,1],
+				  delay: 1000,
+				  duration: 9000
+				});
+
+			 	self.triggerAnimations['bottom'] = true;
+			}
+        },
+
+        /**
+		* animations to run on the start
+        */
         'animations' : function() {
         	
         	// alias
@@ -37,94 +137,6 @@ define('app.display_all_games', ['jquery', 'anime'], function($, Anime){
 				  delay: 1000,
 				  duration: 9000
 				});
-        	});
-
-        	// trigger animations based on scroll
-        	$(window).scroll(function() {
-        		 if ($(this).scrollTop() + 500 > $("#main-description").position().top && !self.triggerAnimations['main-description']) {
-
-        		 	// animate main description, 
-        		 	Anime({
-					  targets: '#main-description div.left-cont',
-					  translateX: [-500, 0],
-					  duration: 6000,
-					  elasticity: 400,
-					});
-
-					Anime({
-					  targets: '#main-description div.right-cont',
-					  translateX: [1000, 0],
-					  duration: 6000,
-					  elasticity: 200,
-					});
-
-					self.triggerAnimations['main-description'] = true;
-        		 } 
-
-        		 // mechanics section
-        		 if ($(this).scrollTop() + 500 > $("#mechanics-title").position().top && !self.triggerAnimations['mechanics']){
-
-        		 	// animation timeline
-        		 	var onebyoneTimeline = Anime.timeline();
-					$('#mechanics-section div.feature-smallsection').each(function(index){
-	        		 	onebyoneTimeline.add({
-						  targets: '#mechanics-section div.feature-smallsection:nth-of-type(' + (index + 1) + ')',
-						  translateY: {
-						      value : [1000, 0],
-						      duration: 2000,
-						  	  elasticity: 500,
-						  },
-						  opacity : {
-						  	  value : [0, 1],
-						  	  duration: 6000,
-						  },
-						  offset: 1000 * index
-						})
-	        		 });
-
-        		 	self.triggerAnimations['mechanics'] = true;
-        		 }
-
-        		 if ($(this).scrollTop() + 500 > $("#gallery-section").position().top && !self.triggerAnimations['gallery']){
-
-        		 	// animation timeline
-        		 	var onebyoneTimelineRotate = Anime.timeline();
-
-        		 	// loop through all gallery elements
-        		 	$('#gallery-section div.gallery_element').each(function(index){
-						onebyoneTimelineRotate.add({
-						  targets: '#gallery-section div.gallery_element:nth-of-type(' + (index + 1) + ')',
-						  rotate: {
-						      value : [360, 0],
-						      duration: 2000,
-						  	  elasticity: 500,
-						  },
-						  opacity : {
-						  	  value : [0, 1],
-						  	  duration: 6000,
-						  },
-						  scale : {
-						  	  value : [0, 1],
-						  	  duration: 6000,
-						  },
-						  offset: 300 * index 
-						})
-        		 	})
-
-        		 	self.triggerAnimations['gallery'] = true;
-        		 }
-
-        		 if ($(this).scrollTop() + 500 > $("#bottom-banner").position().top && !self.triggerAnimations['bottom']){
-
-        		 	Anime({
-					  targets: '#bottom-banner h1, #bottom-banner h4, #bottom-banner button',
-					  opacity: [0,1],
-					  delay: 1000,
-					  duration: 9000
-					});
-
-        		 	self.triggerAnimations['bottom'] = true;
-        		 }
         	});
         }
     }
