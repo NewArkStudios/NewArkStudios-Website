1a42bbc9de4d811346a51bebf832537e54a5f641
lantern77 <peter_hoang2@hotmail.com>
2018-01-05 20:08:50 -0500

    Javascript basic fix for carousel, next/prev and current slide up and running

--- public/js/app/carousel.js
+++ public/js/app/carousel.js
@@ -4,38 +4,70 @@ define('carousel', ['jquery'], function($){
    var application = {
 
         'start' : function(){
+
+            this.slideIndex = 1;
+            
+            // init-event listener
+            this.initEventListener();
+        
             this.showSlides(1);
         },
 
-        'plusSlides' : function(n){
-            showSlides(slideIndex += n);
+        'initEventListener': function() {
+        
+            // alias
+            var self = this;
+
+            $(".slideshow-container a.prev").on('click', function(){
+                self.moveSlides(-1);
+            });
+
+            $(".slideshow-container a.next").on('click', function(){
+                self.moveSlides(1);
+            });
+            
+            $('.slideshow-container span.dot').on('click', function(){
+                var index = $(this).index() + 1;
+                self.currentSlide(index);
+            });
+
+        },
+
+        'moveSlides' : function(n){
+            this.slideIndex += n
+            this.showSlides();
         },
 
         'currentSlide': function(n) {
-            showSlides(slideIndex = n);
+            this.slideIndex = n
+            this.showSlides();
         },
 
-        'showSlides': function(n) {
-          var i;
-          var slides = $(".mySlides");
-          var dots = $(".dot");
-          if (n > slides.length) {
-            slideIndex = 1
-          } 
-          if (n < 1) {
-            slideIndex = slides.length
-          }
+        'showSlides': function() {
+            
+            // alias
+            var self = this;
 
-          // loop through all display and hide
-          $(slides).each(function(index, element){
-            $(element).css('display', 'none');
-          });
+            var i;
+            var slides = $(".mySlides");
+            var dots = $(".dot");
+                if (self.slideIndex > slides.length) {
+              self.slideIndex = 1
+            } 
+            if (self.slideIndex < 1) {
+              self.slideIndex = slides.length
+            }
 
-          for (i = 0; i < dots.length; i++) {
-              dots[i].className = dots[i].className.replace(" active", "");
-          }
-          slides.get(n-1).style.display = "block"; 
-          dots.get(n-1).className += " active";
+            // loop through all display and hide
+            $(slides).each(function(index, element){
+              $(element).css('display', 'none');
+            });
+
+            for (i = 0; i < dots.length; i++) {
+                dots[i].className = dots[i].className.replace(" active", "");
+            }
+            slides.get(self.slideIndex-1).style.display = "block"; 
+            dots.get(self.slideIndex-1).className += " active";
         }
    } 
 
--- resources/views/pages/partials/carousel.blade.php
+++ resources/views/pages/partials/carousel.blade.php
@@ -1,25 +1,27 @@
 <!-- Note remember to include carousel.css and carousel.js -->
+    <!-- The dots/circles -->
+
     <!-- Slideshow container -->
     <div class="slideshow-container">
-
+        <div class="slideshow-footer" style="text-align:center">
+          @foreach ($images as $image)
+              <span class="dot"></span> 
+          @endforeach
+        </div>
       <!-- Full-width images with number and caption text -->
       @foreach ($images as $image)
           <div class="mySlides fade-carousel">
-            <div class="numbertext">1 / 3</div>
             <img src="{{$image}}" style="width:100%">
-            <div class="text">{{$captions[$loop->index]}}</div>
+            <div class="slideshow-caption">
+                <p>{{$captions[$loop->index]}}</p>
+            </div>
           </div>
       @endforeach
 
       <!-- Next and previous buttons -->
-      <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-      <a class="next" onclick="plusSlides(1)">&#10095;</a>
+      <a class="prev" >&#10094;</a>
+      <a class="next" >&#10095;</a>
     </div>
     <br>
 
-    <!-- The dots/circles -->
-    <div style="text-align:center">
-      @foreach ($images as $image)
-          <span class="dot" onclick="currentSlide({{$loop->index}})"></span> 
-      @endforeach
-    </div>
+
